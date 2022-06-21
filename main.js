16716cf647a62582de1595a487f653235065a060
chris <94062641+chrisprof@users.noreply.github.com>
2022-06-20 20:53:52 -0400

    added scroll to increase size

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.module.js"
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 1000, window.innerWidth/window.innerHeight, 0.000000000001, 1000 );
+var camera = new THREE.PerspectiveCamera( 70, window.innerWidth/window.innerHeight, 1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -21,11 +21,17 @@ var animate = function () {
 	requestAnimationFrame( animate );
 
     document.addEventListener('mousemove',function(e){
-        cube.rotation.x=e.clientY/575
-        cube.rotation.y=e.clientX/575
+        cube.rotation.x=e.clientY/525
+        cube.rotation.y=e.clientX/525
+    })
+
+    document.addEventListener('scroll',function(e){
+        camera.position.z=(window.scrollY/window.innerHeight)+5
+        console.log(window.scrollY/window.outerHeight)
     })
 
 	renderer.render( scene, camera );
 };
 
 animate()
+
